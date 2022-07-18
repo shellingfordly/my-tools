4f3005ed69d98a8d0046db0b5a4daca946208885
shellingfordly <shellingfordly@qq.com>
2022-07-18 17:29:33 +0800

    feat: add font-family setting component

---
+++ src/components/font-family/FontFamily.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { FontFamilyOptions } from "./options";
+
+const FontFamilyOptionsFilter = computed(() => {
+  const values = [...new Set(FontFamilyOptions.map((item) => item.value))];
+  const labels = [...new Set(FontFamilyOptions.map((item) => item.label))];
+  return values.map((value, index) => ({
+    value,
+    label: labels[index],
+  }));
+});
+
+function onChange(value: any) {
+  const body = document.body;
+  body.style.fontFamily = value;
+}
+</script>
+
+<template>
+  <a-select
+    @change="onChange"
+    style="width: 200px"
+    allow-search
+    allow-create
+    placeholder="选择字体"
+  >
+    <a-option v-for="item in FontFamilyOptionsFilter" :value="item.value">
+      {{ item.label }}
+    </a-option>
+  </a-select>
+</template>
+
+<style scoped lang="less"></style>
