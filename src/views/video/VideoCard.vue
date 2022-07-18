3b2fe661eba07ac5e9acfeb0936a7d76936d287b
shellingfordly <shellingfordly@qq.com>
2022-07-18 16:11:10 +0800

    feat: add download setting

--- src/views/video/VideoCard.vue
+++ src/views/video/VideoCard.vue
@@ -1,37 +1,64 @@
 <script lang="ts" setup>
-import { FileType } from "/@/hooks/useFFmpeg";
+import { ExportFileType } from "/@/types/video";
 
-defineProps<{
-  url: string;
-  type: FileType;
+const props = defineProps<{
+  file: ExportFileType;
 }>();
 
 const visible = ref(false);
+const size = computed(() => (props.file.size / 1000000).toFixed(2));
+
+const form = reactive({
+  output: "output",
+});
 </script>
 
 <template>
   <div class="card">
     <div class="file">
-      <img v-if="type === 'gif'" :src="url" />
-      <video v-else :src="url" autoplay loop />
+      <img v-if="file.type === 'gif'" :src="file.url" />
+      <video v-else :src="file.url" autoplay loop />
     </div>
     <div class="info">
-      <a-button type="outline" @click="visible = true"> 预览 </a-button>
-      <a-button type="primary" :href="url" :download="`output.${type}`">
-        下载
-      </a-button>
+      <div class="title">文件信息</div>
+      <a-form :model="form">
+        <a-form-item label="大小">
+          <a-input :model-value="size" disabled>
+            <template #suffix>
+              <span>M</span>
+            </template>
+          </a-input>
+        </a-form-item>
+        <a-form-item field="output" label="名字">
+          <a-input v-model="form.output">
+            <template #suffix>
+              <span>.{{ file.type }}</span>
+            </template>
+          </a-input>
+        </a-form-item>
+      </a-form>
+      <div class="btn">
+        <a-button type="outline" @click="visible = true"> 预览 </a-button>
+        <a-button
+          type="primary"
+          :href="file.url"
+          :download="`${form.output}.${file.type}`"
+        >
+          下载
+        </a-button>
+      </div>
     </div>
   </div>
 
   <a-modal
-    :title="`output.${type}`"
+    :title="`output.${file.type}`"
     :visible="visible"
     :footer="false"
     @cancel="visible = false"
     width="80%"
   >
-    <img v-if="type === 'gif'" :src="url" />
-    <video v-else :src="url" autoplay loop />
+    <img v-if="file.type === 'gif'" :src="file.url" />
+    <video v-else :src="file.url" autoplay loop />
   </a-modal>
 </template>
 
@@ -42,30 +69,39 @@ video {
 }
 .card {
   display: flex;
+  padding: 10px;
+  margin-bottom: 10px;
   border: 1px solid #eee;
   border-radius: 10px;
-  margin-bottom: 10px;
 
   .file {
-    width: calc(100% - 200px);
-    padding: 10px;
+    width: calc(100% - 300px);
     display: flex;
     justify-content: center;
   }
   .info {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 200px;
+    width: 300px;
+    padding-left: 50px;
 
-    button {
-      margin-bottom: 20px;
+    .title {
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 50px;
     }
 
-    button,
-    a {
-      width: 100px;
+    .btn {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      button {
+        margin-right: 20px;
+      }
+
+      button,
+      a {
+        width: 100px;
+      }
     }
   }
 }
