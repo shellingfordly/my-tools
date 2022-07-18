e8fbb16c1d4e46290d00ffffbc5636ef0df0b1b6
shellingfordly <shellingfordly@qq.com>
2022-07-18 17:29:04 +0800

    feat: udpate layout

---
+++ src/layout/Header.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import { routes } from "/@/router/modules";
+import FontFamily from "../components/font-family/FontFamily.vue";
+
+const menus = computed(() => {
+  const route = routes.find((v) => v.path === "/home");
+  if (!route) return [];
+  return route.children?.map((r) => ({
+    name: r.meta?.name,
+    path: r.path,
+  }));
+});
+
+const route = useRoute();
+const router = useRouter();
+const selectedKey = ref(route.path);
+
+function onClick(path: string) {
+  router.push(path);
+  selectedKey.value = path;
+}
+</script>
+
+<template>
+  <a-layout-header class="header">
+    <div style="width: 300px"></div>
+    <a-menu
+      class="menu"
+      :selectedKeys="[selectedKey]"
+      mode="horizontal"
+      @menu-item-click="onClick"
+    >
+      <a-menu-item v-for="menu in menus" :key="menu.path">
+        {{ menu?.name }}
+      </a-menu-item>
+    </a-menu>
+    <div class="right">
+      <FontFamily />
+    </div>
+  </a-layout-header>
+</template>
+
+<style scoped lang="less">
+.header {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #eee;
+
+  .right {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
